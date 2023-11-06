23a6e229dabc3d80c1a6a0b54964fccc6d5cc381
ForrestKnight <forrest.knight.yt@gmail.com>
2023-11-06 16:00:35 -0500

    Adjusted styling

--- src/components/Level4/Level4.js
+++ src/components/Level4/Level4.js
@@ -37,7 +37,7 @@ function Level4({ progressToNextLevel }) {
   return (
     <div className="container">
       <h2>Memory Card Game</h2>
-      <p>Your goal is to match pairs of cards with identical symbols. Simply click to flip a card, then click another to find its match. Matched pairs remain face-up, while non-matches flip back down. Win by matching all card pairs!</p>
+      <p className="description">Your goal is to match pairs of cards with identical symbols. Simply click to flip a card, then click another to find its match. Matched pairs remain face-up, while non-matches flip back down. Win by matching all card pairs!</p>
       <div className="card-grid">
         {cards.map((card, index) => (
           <div 
